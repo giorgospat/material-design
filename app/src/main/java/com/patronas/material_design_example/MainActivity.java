22d4f844e6dcfc5a9c20cb8a38dd933247990706
gpatronas <giorgos.patronas@hotmail.com>
2018-12-08 14:53:46 +0200

    floating button implementation

--- app/src/main/java/com/patronas/material_design_example/MainActivity.java
+++ app/src/main/java/com/patronas/material_design_example/MainActivity.java
@@ -1,13 +1,66 @@
 package com.patronas.material_design_example;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isFABOpen= false;
+    private FloatingActionButton fab1, fab2, fab3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initSimpleFloatingButton();
+        initCustomFloatingButton();
+
     }
+
+
+    private void initSimpleFloatingButton() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Fab button clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initCustomFloatingButton() {
+        FloatingActionButton fabCustom = findViewById(R.id.fab_with_menu);
+        fab1 = findViewById(R.id.fab1);
+        fab2 = findViewById(R.id.fab2);
+        fab3 = findViewById(R.id.fab3);
+        fabCustom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isFABOpen){
+                    showFABMenu();
+                }else{
+                    closeFABMenu();
+                }
+            }
+        });
+    }
+
+    private void showFABMenu(){
+        isFABOpen=true;
+        fab1.animate().translationY(-getResources().getDimension(R.dimen.standard_65));
+        fab2.animate().translationY(-getResources().getDimension(R.dimen.standard_130));
+        fab3.animate().translationY(-getResources().getDimension(R.dimen.standard_195));
+    }
+
+    private void closeFABMenu(){
+        isFABOpen=false;
+        fab1.animate().translationY(0);
+        fab2.animate().translationY(0);
+        fab3.animate().translationY(0);
+    }
+
 }
